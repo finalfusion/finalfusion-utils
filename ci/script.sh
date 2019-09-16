ba4fb3a7096776384df91e42aa8429554b02df03
Daniël de Kok <me@danieldk.eu>
2019-09-16 11:39:29 +0200

    CI: only run clippy/rustfmt on current stable

---
+++ ci/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+cargo build --verbose
+cargo test --verbose
+cargo build --verbose --features "opq reductive/openblas"
+
+# On Rust 1.32.0, we only care about passing tests.
+if rustc --version | grep -v "^rustc 1.32.0"; then
+  cargo fmt --all -- --check
+  cargo clippy -- -D warnings
+fi
+
+
