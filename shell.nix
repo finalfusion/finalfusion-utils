d4c933eda3dd4bfface49fc06a0bc88abd6826cb
Daniël de Kok <me@danieldk.eu>
2019-09-18 13:22:47 +0200

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,18 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  name = "finalfusion-utils-env";
+  env = buildEnv { name = name; paths = buildInputs; };
+
+  nativeBuildInputs = [
+    latest.rustChannels.stable.rust
+  ];
+
+  buildInputs = [
+    pkgconfig
+
+    # OpenBLAS backend
+    gfortran.cc.lib
+    openblasCompat
+  ];
+}
