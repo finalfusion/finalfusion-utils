71b640ab27e03b285a56687867995e5c81c49ae1
Daniël de Kok <me@danieldk.eu>
2019-09-19 12:37:54 +0200

    CI: build for x86_64-unknown-linux-musl

--- ci/script.sh
+++ ci/script.sh
@@ -2,14 +2,16 @@
 
 set -ex
 
-cargo build --verbose
-cargo test --verbose
-cargo build --verbose --features "netlib"
+if [ $TARGET = "x86_64-unknown-linux-musl" ]; then
+  cargo build --target ${TARGET} --verbose
+else
+  cargo test --target ${TARGET} --verbose
+  cargo build --target ${TARGET} --features netlib --verbose
 
-# On Rust 1.32.0, we only care about passing tests.
-if rustc --version | grep -v "^rustc 1.32.0"; then
-  cargo fmt --all -- --check
-  cargo clippy -- -D warnings
+  # On Rust 1.32.0, we only care about passing tests.
+  if rustc --version | grep -v "^rustc 1.32.0"; then
+    cargo fmt --all -- --check
+    cargo clippy -- -D warnings
+  fi
 fi
-
 
